04c0a051e445b22a80ca6f1dce5c51c4dc649f2f
Lime <1448918377@qq.com>
2019-08-15 13:39:23 +0800

    注册登录和退出

--- account/urls.py
+++ account/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup,name='sign'),
+    path('login/', views.login,name='log'),
+    path('logout/', views.logout,name='logout'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 
@@ -21,4 +22,22 @@ def signup(request):
 			else:
 				return render(request,'signup.html',{'密码错误':'密码错误，请重新输入'})
 
-			
+
+def login(request):
+	if request.method == 'GET':
+		return render(request,'login.html')
+	elif request.method == 'POST':
+		username = request.POST['用户名']
+		password = request.POST['密码']
+		user = auth.authenticate(username=username,password=password)
+		if user is None:
+			return render(request,'login.html',{ '错误' : '用户名或密码错误' })
+		else:
+			auth.login(request,user)
+			return redirect('home')
+
+def logout(request):
+	if request.method == 'POST':
+		auth.logout(request)
+		return redirect('home')
+
