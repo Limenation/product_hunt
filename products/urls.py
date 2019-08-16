a23602984860dd4f2f8b9343cf8d488da4f8c69d
Lime <1448918377@qq.com>
2019-08-16 17:02:48 +0800

    发布的详情界面

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('publish',views.publish,name='publish')
+	path('publish',views.publish,name='publish'),
+	path('<int:product_id>',views.detail,name='detail'),
 ]
